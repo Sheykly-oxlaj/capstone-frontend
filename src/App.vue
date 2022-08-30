926c8910a1b6e722182e0869c8aeb593a25556ef
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-08-30 10:46:22 -0700

    adds Journals Index component and route

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,8 @@
     <router-link to="/login">Login</router-link>
     |
     <router-link to="/logout">Logout</router-link>
+    |
+    <router-link to="/journals">All my Journals</router-link>
   </nav>
   <router-view />
 </template>
---
+++ src/views/JournalsIndex.vue
@@ -0,0 +1,35 @@
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "All Journals!",
+      journals: [],
+    };
+  },
+  created: function () {
+    this.indexJournal();
+  },
+  methods: {
+    indexJournal: function () {
+      axios.get("/journals").then((response) => {
+        console.log("journals index", response);
+        this.journals = response.data;
+      });
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+    <div v-for="journal in journals" v-bind:key="journal.id">
+      <router-link :to="`/journals/${journal.id}`">
+        <h2>{{ journal.name }}</h2>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<style></style>
