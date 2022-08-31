87b9d593f57343a2cc8dca9b5e79e3bcdfc713e8
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-08-31 11:10:00 -0700

    deletes journal destroy action

--- src/views/JournalsShow.vue
+++ src/views/JournalsShow.vue
@@ -12,14 +12,14 @@ export default {
       this.journal = response.data;
     });
   },
-  methods: {
-    destroyJournal: function () {
-      axios.delete("/journals/" + this.$route.params.id + ".json").then((response) => {
-        console.log("IT GONE!", response.data);
-        this.$router.push("/journals");
-      });
-    },
-  },
+  // methods: {
+  //   destroyJournal: function () {
+  //     axios.delete("/journals/" + this.$route.params.id + ".json").then((response) => {
+  //       console.log("IT GONE!", response.data);
+  //       this.$router.push("/journals");
+  //     });
+  //   },
+  // },
 };
 </script>
 
@@ -35,7 +35,8 @@ export default {
     <router-link v-bind:to="`/journal-entries`">Add Journal Entry</router-link>
     <div>
       <!-- uses v-bind since the links have dynamic attributes… -->
-      <button v-on:click="destroyJournal()">Delete this Journal</button>
+      <!-- <button v-on:click="destroyJournal()">Delete this Journal</button> -->
+      <!-- I dont have a delete function -->
     </div>
   </div>
 </template>
