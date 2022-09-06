bd33dc8854dc5c56e3a2b6ad37e836bac24aa4ea
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-09-06 11:03:35 -0700

    adds navbar to the whole thing

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,30 @@
     <router-link to="/journal-entries/:id">Entry</router-link> -->
   <!-- displayed in journalsIndex/JournalsShow/ -->
   <!-- </nav> -->
+  <section id="header">
+    <div class="container">
+      <!-- Logo -->
+      <h1 id="logo">
+        <router-link to="/login">FOR ME | BY ME</router-link>
+      </h1>
+      <p>A PLACE TO RECORD MY THOUGHTS</p>
+
+      <!-- Nav -->
+      <nav id="nav">
+        <h1></h1>
+
+        <p>
+          <a href="/">Home</a>
+          |
+          <a href="/login">Login</a>
+          |
+          <a href="/logout">Logout</a>
+          |
+          <a href="/journals">All my Journals</a>
+        </p>
+      </nav>
+    </div>
+  </section>
 
   <router-view />
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,28 +3,7 @@
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div> -->
-  <section id="header">
-    <div class="container">
-      <!-- Logo -->
-      <h1 id="logo">
-        <router-link to="/login">FOR ME | BY ME</router-link>
-      </h1>
-      <p>A PLACE TO RECORD MY THOUGHTS</p>
-
-      <!-- Nav -->
-      <nav id="nav">
-        <h1></h1>
-
-        <p>
-          <a href="/login">Login</a>
-          |
-          <a href="/logout">Logout</a>
-          |
-          <a href="/journals">All my Journals</a>
-        </p>
-      </nav>
-    </div>
-  </section>
+  <section></section>
 </template>
 
 <script>
