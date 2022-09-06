2252f8649b6aa1489a1742fce4bffc70fa663c0a
Sheykly Oxlaj <oxlajsheylt@gmail.com>
2022-09-06 12:55:40 -0700

    adds navbar to journals index

--- src/views/JournalsIndex.vue
+++ src/views/JournalsIndex.vue
@@ -22,6 +22,34 @@ export default {
 </script>
 
 <template>
+  <div id="page-wrapper">
+    <!-- Header -->
+    <section id="header">
+      <div class="container">
+        <!-- Logo -->
+        <h1 id="logo">
+          <router-link to="/login">FOR ME | BY ME</router-link>
+        </h1>
+        <p>A PLACE TO RECORD MY THOUGHTS</p>
+
+        <!-- Nav -->
+        <nav id="nav">
+          <h1></h1>
+
+          <p>
+            <a href="/">Home</a>
+            |
+            <a href="/login">Login</a>
+            |
+            <a href="/logout">Logout</a>
+            |
+            <a href="/journals">All my Journals</a>
+          </p>
+        </nav>
+      </div>
+    </section>
+  </div>
+
   <div class="journals">
     <h1>{{ message }}</h1>
     <div v-for="journal in journals" v-bind:key="journal.id">
